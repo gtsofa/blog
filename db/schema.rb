2439405213d2dab07a00c9d737d25197de5b8371
Julius Nyule <jlsnyule@yahoo.com>
2019-08-10 12:01:53 +0300

    create profile for a user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_212716) do
+ActiveRecord::Schema.define(version: 2019_08_09_221354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 2019_08_07_212716) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "twitter"
+    t.string "github"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "subscribers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -56,4 +65,5 @@ ActiveRecord::Schema.define(version: 2019_08_07_212716) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "profiles", "users"
 end
